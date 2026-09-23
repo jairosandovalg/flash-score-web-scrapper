91137498fe8e0fa9e77693c2f3c93a1efb6b6221
JairoSG <jairosandovalg011@gmail.com>
2026-09-23 12:11:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def ():
       
       browser.close()
 
-if __name__ == "__main__":                                             #Asegurar que el bloque de código debajo de ella solo se ejecute
+if __name__ == "__main__":                               #Asegurar que el bloque de código debajo de ella solo se ejecute
                                                          #cuando el archivo se corre directamente, y no cuando se importa 
                                                          #como un módulo desde otro script de Python.
 
