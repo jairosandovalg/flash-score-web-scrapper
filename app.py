73aeb4a54e4b9449241a69393c3735f2a4d43420
JairoSG <jairosandovalg011@gmail.com>
2026-09-23 11:14:17 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+#========================================================
+#Cargar librerias
+#========================================================
+from playwright.sync_api import sync_playwright
+
+#========================================================
+#Configurar el navegador
+#========================================================
+def ():
+  with sync_playwright() as p:
+      browser = p.chromium.launch(headless=True)
+      page = browser.new_page()
+      page.goto("https://example.com")
+      
+      # Extraer contenido tras la ejecución de JS
+      titulo = page.locator("h1").inner_text()
+      print(titulo)
+      
+      browser.close()
