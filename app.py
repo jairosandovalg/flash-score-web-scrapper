b05bf429767ae6ebed8722b1a8782f6022381834
JairoSG <jairosandovalg011@gmail.com>
2026-09-26 11:18:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,11 @@ def partidos():
     #==================================================================
     #Extraer Estadísticas Principales
     #================================================================== 
-    tab_stats = page.locator('a[data-analytics-alias="match-statistics"], a:has-text("ESTADÍSTICAS"), button:has-text("ESTADÍSTICAS")').first
+    tab_stats = page.locator(
+                                'a[data-analytics-alias="match-statistics"], '
+                                'a:has-text("ESTADÍSTICAS"), '
+                                'button:has-text("ESTADÍSTICAS")'
+                            ).first
 
 
 
